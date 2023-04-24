5773f5985ffc9194cc6b8c6e363f3b031bb2bbb2
apazorin <andrea.perez.azorin@accenture.com>
2023-04-24 18:17:27 +0200

    feat(routes): add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'events',
+    loadChildren: () => import('./events/events-routing.module').then( m => m.EventsRoutingModule),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'events',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/events/events-routing.module.ts
+++ src/app/events/events-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillboardPageComponent } from './pages/billboard-page/billboard-page.component';
+import { EventDetailsPageComponent } from './pages/event-details-page/event-details-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BillboardPageComponent
+  },
+  {
+    path: 'details',
+    component: EventDetailsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
